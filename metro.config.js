506e58102809eade35ff2d255ebb07db3cc1d80a
Lance Cochangco <lancecochangco@gmail.com>
2026-09-07 15:12:45 +0800

    refactor: remove unused web desktop configurations and scripts

--- metro.config.js
+++ metro.config.js
@@ -3,12 +3,4 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.assetExts.push("wasm");
-
-config.resolver.sourceExts.push("sql");
-
-config.transformer.asyncRequireModulePath = require.resolve(
-  "metro-runtime/src/modules/asyncRequire",
-);
-
 module.exports = withNativeWind(config, { input: "./src/app/global.css" });
